989867ae1169d827993f7c3ffc154f370bcc34bf
Brice GOUDALO <bricegoudalo@gmail.com>
2022-07-24 23:55:50 +0000

    add routes

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # real-world-vue
+```
+It's a simple app that helps out the world on event at a time. As you can see, we're displaying a list of events, which we're pulling in through an API call and when we click on an event, we can see the event details. Additionally, we can route to the about page.
+```
 
 ## Project setup
 ```
---
+++ src/components/EventCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="event-cart">
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  /* eslint-disable */
+  name: "EventCard",
+  props: {
+    event: Object,
+  },
+};
+</script> 
+
+<style scoped>
+  .event-cart {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+  }
+  .event-cart:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  }
+</style>
+  
